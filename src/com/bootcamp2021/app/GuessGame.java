3b4cd879e7e9e59f0d4572f17440cafc303ffa2e
LeonardUK1980 <nema.otto@gmail.com>
2021-06-30 09:37:10 +0100

    add try-catch block

--- src/com/bootcamp2021/app/GuessGame.java
+++ src/com/bootcamp2021/app/GuessGame.java
@@ -1,5 +1,6 @@
 package com.bootcamp2021.app;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,7 +14,6 @@ public class GuessGame {
     public GuessGame(){
         this.random = new Random().nextInt(101 - 1) + 1;
         this.sc = new Scanner(System.in);
-        System.out.println(random);
     }
 
     private void validateGuess(){
@@ -34,27 +34,36 @@ public class GuessGame {
 
     public void play(){
         // request the 1st guess
-        setGuess();
-        while (this.round != 0){
-            if (this.guess > this.random){
-                System.out.println("Please pick a lower number");
-                setGuess();
-            }else if (this.guess  < this.random){
-                System.out.println("Please pick a higher number!");
-                setGuess();
-            }else{
-                System.out.println("You won!");
-            return ;
+        try{
+            setGuess();
+            while (this.round != 0){
+                if (this.guess > this.random){
+                    System.out.println("Please pick a lower number");
+                    setGuess();
+                }else if (this.guess  < this.random){
+                    System.out.println("Please pick a higher number!");
+                    setGuess();
+                }else{
+                    System.out.println("You won!");
+                    return ;
+                }
             }
-        };
-        // for the last guess we do not give the option to choose lower or higher number
-        // hence the last guess does not goes through the validation process, as the result here we just check
-        // whether it is matches or not
-        if (this.guess == this.random){
-            System.out.println("You won!");
-        }else{
-            System.out.println("You lose, the number to guess was " + this.random);
+            // for the last guess we do not give the option to choose lower or higher number
+            // hence the last guess does not goes through the validation process, as the result here we just check
+            // whether it is matches or not
+            if (this.guess == this.random){
+                System.out.println("You won!");
+            }else{
+                System.out.println("You lose, the number to guess was " + this.random);
+            }
+            System.out.println(random);
+        }catch(Exception e){
+            System.out.println("Something went wrong!");
+            System.out.println(e.getMessage());
+        }finally {
+            sc.close();
         }
+
 
     }
 
